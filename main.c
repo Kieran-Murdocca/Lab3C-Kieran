189f4ca9e0f7867ff85058b27b55f1699fbf03e2
KieranMurdocca <kkm5754@psu.edu>
2020-09-18 02:24:02 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,46 @@
+// Author: Kieran Murdocca kkm5754@psu.edu
+// Collaborator: Augustus Perseghin agp5191@psu.edu
+// Collaborator: Zehao Liu zql5426@psu.edu
+// Collaborator: Nidhi Swamy nms6241@psu.edu
+// Section: 004
+// Breakout: 15
+// Part 1: lab3-c
+
+#include <stdio.h>
+#include <readline/readline.h>
+#include <stdlib.h>
+
+int sum_n(int n);
+void print_n(const char *s, int n);
+
+int main() {
+  int num;
+  char str[50];
+  printf("Enter an int: ");
+  // processing sum
+  scanf("%d", &num);
+  printf("sum is %d.", sum_n(num));
+  // processing string
+  printf("\nEnter a string: ");
+  scanf("%s", str);
+  print_n(str, num);
+}
+
+int sum_n(int n) {
+  if (n>0) {
+    return n + sum_n(n-1);
+  }
+   else {
+    return 0;
+  }
+}
+
+void print_n(const char *s, int n){
+  if (n>0) {
+    printf("\n%s", s);
+    print_n(s, n-1);
+  }
+  else {
+    return; 
+  }
+}
