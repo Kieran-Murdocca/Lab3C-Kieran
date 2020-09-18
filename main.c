9301b1e81679c0aa3adc3ce684773d6e5caa2e98
KieranMurdocca <kkm5754@psu.edu>
2020-09-18 03:50:41 +0000

    Redesign

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 // Collaborator: Augustus Perseghin agp5191@psu.edu
 // Collaborator: Zehao Liu zql5426@psu.edu
 // Collaborator: Nidhi Swamy nms6241@psu.edu
+// Collaborator: Peter Schulman pks5481@psu.edu
 // Section: 004
 // Breakout: 15
 // Part 1: lab3-c
@@ -10,37 +11,26 @@
 #include <readline/readline.h>
 #include <stdlib.h>
 
-int sum_n(int n);
-void print_n(const char *s, int n);
-
-int main(void) {
-  int num=0;
-  char str[50];
-  printf("Enter an int: ");
-  // processing sum
-  scanf("%d", &num);
-  printf("%d\nsum is %d.", num, sum_n(num));
-  // processing string
-  printf("\nEnter a string: ");
-  scanf("%s", str);
-  print_n(str, num);
-}
-
 int sum_n(int n) {
-  if (n>0) {
-    return n + sum_n(n-1);
-  }
-   else {
-    return 0;
-  }
+  if (n == 0) {
+    return 0;}
+    else {
+      return(n+ sum_n(n-1));
+    }
 }
 
-void print_n(const char *s, int n){
+void print_n(const char *s, int n) {
   if (n>0) {
-    printf("\n%s", s);
+    printf("%s\n", s);
     print_n(s, n-1);
   }
-  else {
-    return; 
-  }
+}
+
+int main(void) {
+  int n = atof(readline("Enter an int: "));
+  printf("sum is %d.\n", sum_n(n));
+
+  char *s = readline("Enter a string: ");
+  print_n(s, n); 
+return 0; 
 }
